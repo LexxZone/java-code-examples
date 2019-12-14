55548244752ec65bd242e2963c1893376cd33b9f
Alexey Dvoryaninov <alexey.dvoryaninov@devexperts.com>
2019-12-14 04:34:11 +0300

    Added some project files

---
+++ src/main/java/ru/stateofmind/codeexamples/ExamplesOfCodeApplication.java
@@ -0,0 +1,13 @@
+package ru.stateofmind.codeexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamplesOfCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamplesOfCodeApplication.class, args);
+	}
+
+}
